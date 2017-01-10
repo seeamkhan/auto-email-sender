a2fa88306d7e310ff3fe5777bf3f3d9fbce403d0
seeam <zafi005@gmail.com>
2017-01-11 01:43:56 +0600

    added general script

--- general.py
+++ general.py
@@ -1,18 +1,14 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from __builtin__ import classmethod
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.select import Select
-import unittest, time, re
-import os
-from selenium.webdriver.common.action_chains import ActionChains
+import time
 from selenium.webdriver.support import expected_conditions as EC
-import Conf_Reader
 
 
 def email_sender():
+    print 'The automated email is sending, please wait...'
     driver = webdriver.Chrome()
     driver.maximize_window()
     base_url = 'https://gmail.com/'
@@ -28,7 +24,7 @@ def email_sender():
 
     # enter password
     driver.find_element_by_id('Passwd').clear()
-    driver.find_element_by_id('Passwd').send_keys('test1111')
+    driver.find_element_by_id('Passwd').send_keys('')
 
     # click login
     driver.find_element_by_id('signIn').click()
@@ -46,12 +42,11 @@ def email_sender():
 
     # verify composer box appears
 
-    # to_field = "//textarea[contains(@id, ':nr')]"
-    # to_field = "//textarea"
     to_field = ".//*[@id=':nk']"
     subject_field = ".//*[@id=':n4']"
     body_field = ".//*[@id=':oa']"
     send_button = ".//*[@id=':mu']"
+    sent_confirmation = ".//*[@id='link_vsm']"
 
     try:
         WebDriverWait(driver, 10).until(
@@ -61,22 +56,31 @@ def email_sender():
         print 'To field not found, maybe composer box load failed!'
 
     # Enter data into to field
-    for i in xrange(4):
-        try:
-            driver.find_element_by_xpath(to_field).clear()
-            break
-        except:
-            print "Unknown Browse Server button."
-    # driver.find_elements_by_id(to_field).clear()
-    driver.find_element_by_xpath(to_field).send_keys('jacksparrow010203@gmail.com')
+    # for i in xrange(4):
+    #     try:
+    #
+    #         break
+    #     except:
+    #         print "Unknown Browse Server button."
+    driver.find_element_by_xpath(to_field).clear()
+    driver.find_element_by_xpath(to_field).send_keys('jacksparrow010203@gmail.com; zafi005@gmail.com')
     driver.find_element_by_xpath(subject_field).clear()
-    driver.find_element_by_xpath(subject_field).send_keys('Test automated email')
+    driver.find_element_by_xpath(subject_field).send_keys('SINP IS OPEN')
     driver.find_element_by_xpath(body_field).clear()
-    driver.find_element_by_xpath(body_field).send_keys('Body of the automated test email')
-    driver.find_element_by_xpath(send_button).click()
+    driver.find_element_by_xpath(body_field).send_keys('This is an automated alert email for Seeam and Saathi to notify that SINP has been opened. Good Luck!')
 
+    time.sleep(5)
+    quit()
+    driver.find_element_by_xpath(send_button).click()
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, sent_confirmation)))
+    except:
+        print 'Email sent failed!'
 
     time.sleep(5)
     driver.quit()
 
+
+
 email_sender()
+raw_input("Automated email has been sent.\nPress enter to exit...")
